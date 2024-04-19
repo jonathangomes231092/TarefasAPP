f0eb0cd16901df0c52ed6fd87be0901270baaaae
jonathan231092@gmail.com <jonathan231092@gmail.com>
2024-04-19 08:50:37 -0300

    autenticação e criação de usuários, cadastro, consulta, exclusão e edição de tarefas.

---
+++ src/app/models/usuarios/cria-usuario.response.ts
@@ -0,0 +1,10 @@
+/*
+    Modelo de dados para a resposta (retorno de dados)
+    do endpoint: /api/usuarios/criar
+*/
+export interface CriarUsuarioResponse {
+    id: string;
+    nome: string;
+    email: string;
+  }
+  
